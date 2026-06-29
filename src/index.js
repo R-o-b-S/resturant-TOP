38d6994b7d8487b1292640ae2ce0d47264f46fde
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-06-30 00:01:27 +0200

    added a clearContent function to remove content before rendering a new button, tested using menu button

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import "./styles.css";
 import "./home.js";
 
 console.log ("test");
+
+function clearContent () {
+    const myNode = document.getElementById("content");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.lastChild);
+    } 
+}
+
+document.getElementById("menu").onclick = clearContent;
