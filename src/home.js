e769d4e713fe85ed3c3f66cc5b59389c6d35a965
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-06-29 23:56:15 +0200

    home now build by JS

--- src/home.js
+++ src/home.js
@@ -1,2 +1,28 @@
 import "./home.css";
 
+
+function buildHome () {
+    const div = document.createElement("div");
+    div.id = "homeC";
+    document.getElementById("content").appendChild(div);
+
+    const div2 = document.createElement("div");
+    div2.id = "magnaName";
+    let txt = "Magna e zitto!";
+    div2.textContent = txt;
+    document.getElementById("homeC").appendChild(div2);
+
+    const div3 = document.createElement("div");
+    div3.id = "catch";
+    txt = "Autentic Marche cuisine";
+    div3.textContent = txt;
+    document.getElementById("homeC").appendChild(div3);
+
+    const div4 = document.createElement("div");
+    div4.id = "smallerCatch";
+    txt = "You will be fed like we are your nonna";
+    div4.textContent = txt;
+    document.getElementById("homeC").appendChild(div4);
+}
+
+buildHome ();
